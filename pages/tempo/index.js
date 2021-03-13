b87858d69e9d9ac29cbca9b1af96fcf831064fa3
KaioViana <kaiu.viana@hotmail.com>
2021-03-13 19:19:37 -0300

    semantica reajustada

--- pages/tempo/index.js
+++ pages/tempo/index.js
@@ -3,23 +3,23 @@ import React from 'react'
 
 export default function Tempo(props) {
   const data = new Date()
-  const dataStringStatic = data.toGMTString()
+  const dataStringDynamic = data.toGMTString()
 
   return (
     <>
-      <h1>{dataStringStatic} (Static)</h1>
-      <h1>{props.dataStringDynamic} (Dynamic)</h1>
+      <h1>{props.dataStringStatic} (Dynamic)</h1>
+      <h1>{dataStringDynamic} (Static)</h1>
     </>
   )
 }
 
 export function getStaticProps() {
   const data = new Date()
-  const dataStringDynamic = data.toGMTString()
+  const dataStringStatic = data.toGMTString()
 
   return {
     props: {
-      dataStringDynamic
+      dataStringStatic
     },
     revalidate: 5
   }
