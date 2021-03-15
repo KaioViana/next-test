08635b7ea8340527aa8ae3bd2e7e5f4d05d23840
KaioViana <kaiu.viana@hotmail.com>
2021-03-15 07:38:16 -0300

    data

--- pages/tempo/index.js
+++ pages/tempo/index.js
@@ -7,7 +7,7 @@ export default function Tempo(props) {
   
   return (
     <>
-      <h1>{props.res.data.contatos[0].titulo} (static)</h1>
+      <h1>{props.apiData.contatos[0].titulo} (static)</h1>
       <h1>{data.contatos[0].titulo} (dynamic)</h1>
       <h1>{props.dataString} (static)</h1>
     </>
@@ -19,6 +19,7 @@ export async function getStaticProps() {
   
   const data = new Date()
   const dataString = data.toGMTString()
+  
   const res = await api.get('geral')
   const apiData = res.data
   console.log('StaticProps')
